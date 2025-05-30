0709ad1f2ce97490dbf3f4fce35e11f6bc568c74
Hinata0607 <atahin0607@gmail.com>
2025-05-31 01:54:33 +0900

    add: Develop a hook to recursively scan a specific directory in a specified repository

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,6 +1,17 @@
+import { useGithubFileTreeFunction } from '@/hook';
 import { Box } from '@mui/material';
+import { useEffect } from 'react';
 
 export const TopPage = () => {
+	useEffect(() => {
+		const f = async () => {
+			const name = await useGithubFileTreeFunction(
+				'https://github.com/hayasin2004/HackathonVol4',
+			);
+			console.log(name);
+		};
+		f();
+	});
 	return (
 		<Box
 			width={300}
