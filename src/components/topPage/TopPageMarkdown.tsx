a6e55645f8e14c83582dcf9841d8479c5c19a4a1
atsuya <f.atsuya.personal@gmail.com>
2025-06-05 18:05:26 +0900

    エラーが出ると豆腐表示されることはなくなったが、Errorが出るとMarkdown表示がpreviewとしてではなくtextとして表示されてしまう。また、successした時は全文完成してからmarkdownになってしまう

--- src/components/topPage/TopPageMarkdown.tsx
+++ src/components/topPage/TopPageMarkdown.tsx
@@ -1,22 +1,26 @@
 import type { TopPageMarkdownProps } from '@/interfaces';
+import { fixMarkdownContent } from '@/utils/fixMarkdownContent';
 import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 export const TopPageMarkdown = ({ content }: TopPageMarkdownProps) => {
 	const [typedContent, setTypedContent] = useState<string>('');
+	const [fixedContent, setFixedContent] = useState<string>('');
 
 	useEffect(() => {
 		setTypedContent('');
 		let index = 0;
+		const fixed = fixMarkdownContent(content);
+		setFixedContent(fixed);
 
 		const interval = setInterval(() => {
 			setTypedContent((prev) => {
-				if (index >= content.length) {
+				if (index >= fixed.length) {
 					clearInterval(interval);
 					return prev;
 				}
-				const nextChar = content[index];
+				const nextChar = fixed[index];
 				index++;
 				return prev + nextChar;
 			});
@@ -25,11 +29,26 @@ export const TopPageMarkdown = ({ content }: TopPageMarkdownProps) => {
 		return () => clearInterval(interval);
 	}, [content]);
 
+	useEffect(() => {
+		console.log('typedContent===>', typedContent);
+	}, [typedContent]);
+
+	// return (
+	// 	<div style={{ width: '100%' }}>
+	// 		<ReactMarkdown remarkPlugins={[remarkGfm]}>
+	// 			{String(typedContent)}
+	// 		</ReactMarkdown>
+	// 	</div>
+	// );
 	return (
 		<div style={{ width: '100%' }}>
-			<ReactMarkdown remarkPlugins={[remarkGfm]}>
-				{String(typedContent)}
-			</ReactMarkdown>
+			{typedContent === fixedContent ? (
+				<ReactMarkdown remarkPlugins={[remarkGfm]}>
+					{typedContent}
+				</ReactMarkdown>
+			) : (
+				<pre style={{ whiteSpace: 'pre-wrap' }}>{typedContent}</pre>
+			)}
 		</div>
 	);
 };
---
+++ src/utils/fixMarkdownContent.ts
@@ -0,0 +1,22 @@
+import { micromark } from 'micromark';
+
+export const fixMarkdownContent = (row: string): string => {
+	let fixed = row;
+
+	// ```の数が奇数なら閉じ忘れと判断
+	// ```を末尾に追加
+	// これがないと、Markdown構築時、最初の```が入力された時にエラーになります
+	const backtickCount: number = (row.match(/```/g) || []).length;
+	if (backtickCount % 2 === 1) {
+		fixed += '\n```';
+	}
+
+	// micromarkで構文チェック
+	try {
+		micromark(fixed);
+		return fixed;
+	} catch (e) {
+		console.error('Markdown構文エラー：', e);
+		return `${fixed}\n\n Markdown構文にエラーがあります`;
+	}
+};
